126e155008722ee614a27e955699708ac6f0e2f0
Cheruiyot4008 <Cheruiyotbrian454@gmail.com>
2024-08-21 19:50:20 +0300

    checking for the least count of the number of coins

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,29 +1,24 @@
 #!/usr/bin/python3
-"""Determine the fewest number of coins
-needed to meet a given amount total
-"""
+
+""" Contains makeChange function"""
 
 
 def makeChange(coins, total):
-    """Return fewest number of coins needed to meet total
-    0 if total is 0 or less, -1 if total cannot be met
     """
+    Returns: fewest number of coins needed to meet total
+        If total is 0 or less, return 0
+        If total cannot be met by any number of coins you have, return -1
+    """
+    if not coins or coins is None:
+        return -1
     if total <= 0:
         return 0
-
-    # Coins validity check
-    if (coins is None or len(coins) == 0):
-        return -1
-
     change = 0
-    available_coins = sorted(coins, reverse=True)
-    change_left = total
-
-    for coin in available_coins:
-        while (change_left % coin >= 0 and change_left >= coin):
-            change += int(change_left / coin)
-            change_left = change_left % coin
-
-    change = change if change_left == 0 else -1
-
-    return change
+    coins = sorted(coins)[::-1]
+    for coin in coins:
+        while coin <= total:
+            total -= coin
+            change += 1
+        if (total == 0):
+            return change
+    return -1
